300f8c0ab25b0669f6daa9dbcec7a03a477c4091
Petr Krysl <pkrysl@ucsd.edu>
2020-12-24 11:31:58 -0800

    plot timing

--- src/FinEtoolsRapidHarmonicVA.jl
+++ src/FinEtoolsRapidHarmonicVA.jl
@@ -5,7 +5,7 @@ export getnamewext, retrieve_json, store_json, retrieve_matrix, store_matrix, wi
 include("ModelUtilities.jl")
 export model_setup, full_free_vibration, show_mesh, full_harmonic_vibration, full_harmonic_vibration_direct, redu_free_vibration
 include("GraphicsUtilities.jl")
-export plot_mesh, plot_frf
+export plot_mesh, plot_frf, plot_timing
 
 
 end # module
--- src/GraphicsUtilities.jl
+++ src/GraphicsUtilities.jl
@@ -52,3 +52,34 @@ function plot_frf(sim_list = ["sim1"])
             ))
     display(pl)
 end
+
+function plot_timing(sim_list = ["sim1"], stage = "harmonic_vibration")
+    colors = ["rgb(15, 15, 15)", "rgb(125, 15, 15)", "rgb(15, 125, 15)", "rgb(15, 15, 125)"]
+    traces = AbstractTrace[]
+
+    for sim in sim_list
+        prop = retrieve_json(sim)
+        # Load the data for the graph of the FRF
+        j = joinpath(prop["resultsdir"], sim * "-results" * ".json")
+        results = retrieve_json(j)
+        hvd = results[stage] 
+        # Unwrap the data
+        tims = hvd["timing"]
+        kys = keys(tims)
+        ts = [tims[k] for k in keys(tims)] 
+        c = bar(;x=kys, y=ts, line_color = pop!(colors), name=sim)
+        push!(traces,  c)
+    end
+    
+    # Plot the amplitude of the FRF.
+
+    # 
+    layout = Layout(;autosize = true, xaxis=attr(title="Operation [ND]"), yaxis=attr(title="Time [s]"))
+    # Plot the graphs:
+    #c1 = scatter(;x=frequencies, y=umidAmpl, mode="lines", name = "$(sensortag)", line_color = "rgb(15, 15, 15)")
+    pl = plot(traces, layout; options = Dict(
+            :showSendToCloud=>true, 
+            :plotlyServerURL=>"https://chart-studio.plotly.com"
+            ))
+    display(pl)
+end
