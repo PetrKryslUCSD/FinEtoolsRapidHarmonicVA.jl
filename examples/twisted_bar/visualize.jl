d9119bf7323f190cd29c65793c1d272800547259
Petr Krysl <pkrysl@ucsd.edu>
2021-04-18 10:40:52 -0700

    visualize

--- examples/twisted_bar/visualize.jl
+++ examples/twisted_bar/visualize.jl
@@ -10,20 +10,19 @@ using Plotting: plot_timing_reduced_basis, plot_frf_errors, plot_frf_amplitudes
 using FinEtoolsRapidHarmonicVA
 
 
-methods = [("none", "direct"), ("free", "modal"), ("two_stage_free", "modal"), ("wyd_ritz", "modal"), ("two_stage_wyd_ritz", "modal")]
-#methods = [("free", "modal"), ("two_stage_free", "modal"), ("wyd_ritz", "modal"), ("two_stage_wyd_ritz", "modal")]
+the_methods = [("none", "direct"), ("free", "modal"), ("two_stage_free", "modal"), ("wyd_ritz", "modal"), ("two_stage_wyd_ritz", "modal")]
+the_methods = [("free", "modal"), ("two_stage_free", "modal"), ("wyd_ritz", "modal"), ("two_stage_wyd_ritz", "modal")]
 
-for mesh_n in [8, ]
-    for nmodes in [25, ]
+for mesh_n in [10, ]
+    for nmodes in [200, ]
         
         sims = []
-        for (reduction_method, harmonic_method) in methods
+        for (reduction_method, harmonic_method) in the_methods
             sim = define_sim(; mesh_n = mesh_n, nmodes = (harmonic_method == "direct" ? 0 : nmodes), reduction_method = reduction_method, harmonic_method = harmonic_method)
             push!(sims, sim)
         end
         
         plot_frf_amplitudes(sim_directory(), sims, "frf-m$(mesh_n)-n$(nmodes).pdf")
-        plot_frf_errors(sim_directory(), sims, "frf-errors-m$(mesh_n)-n$(nmodes).pdf")
         
         #plot_frf_errors([sim0, sim1, sim2, sim3,  ], "frf-errors-m$(mesh_n)-n$(nmodes).pdf")
         #plot_frf_errors([sim1,], "frf-m$(mesh_n)-n$(nmodes).pdf")
