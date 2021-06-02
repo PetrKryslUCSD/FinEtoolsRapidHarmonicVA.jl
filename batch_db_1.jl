de1ade41006ad6cda8d7d11abe866868d6c0b820
Petr Krysl <pkrysl@ucsd.edu>
2021-06-02 10:28:38 -0700

    new batch file

---
+++ batch_db_1.jl
@@ -0,0 +1,41 @@
+using Pkg; Pkg.activate("."); Pkg.instantiate()
+using Revise
+
+#using FinEtoolsRapidHarmonicVA
+
+@info "FinEtoolsRapidHarmonicVA ready"
+
+include("examples/brake_disc/define_sim.jl")
+
+reduction_methods = ["free", "two_stage_free_enh", "two_stage_free", "two_stage_wyd_ritz", "wyd_ritz"]
+
+@info "Burn in "
+
+linsolve_method = "minres"
+for itmax in [5]
+    for mesh_n in [1, ]
+        for nmodes in [50, ]
+            for reduction_method in reduction_methods
+                sim = define_sim(; mesh_n = mesh_n, nmodes = nmodes, reduction_method = reduction_method, linsolve_method = linsolve_method, itmax = itmax)
+                solve(sim_directory(), sim, make_model)
+            end
+        end
+    end
+end
+
+
+@info "Running simulations"
+
+##Now for real
+
+linsolve_method = "minres"
+for mesh_n in [4, ]
+    for itmax in [5, 10, 20, 40]
+        for nmodes in [100, 200, 400]
+            for reduction_method in reduction_methods
+                sim = define_sim(; mesh_n = mesh_n, nmodes = nmodes, reduction_method = reduction_method, linsolve_method = linsolve_method, itmax = itmax)
+                solve(sim_directory(), sim, make_model)
+            end
+        end
+    end
+end
